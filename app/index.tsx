d6abd10888b0882bc1b4b3df9953bdac3c8d959f
Camaria Bevavy <bcamaria@gmail.com>
2025-09-18 14:31:01 -0700

    Refactor PublicHome layout and enhance ClaimSighting component with ScrollView for better usability (#13)
    Co-authored-by: Camaria Bevavy <cabevavy@microsoft.com>

--- app/index.tsx
+++ app/index.tsx
@@ -8,42 +8,46 @@ export default function PublicHome() {
 
   return (
     <View style={styles.container}>
-      <HomePageHeader />
+      <View style={styles.logoContainer}>
+        <HomePageHeader />
+      </View>
 
-      <Button
-        icon="paw"
-        mode="contained"
-        onPress={() => router.push("/sightings/new")}
-        contentStyle={{ width: "100%" }}
-        style={styles.button}
-      >
-        Report a Pet Sighting
-      </Button>
-      <Button
-        icon="paw"
-        mode="outlined"
-        onPress={() => router.push("/sightings/")}
-        contentStyle={{ width: "100%" }}
-        style={styles.button}
-      >
-        View Lost Pet Sightings
-      </Button>
-      <Button
-        icon=""
-        mode="outlined"
-        onPress={() => router.push("/(auth)/signin")}
-        style={styles.button}
-      >
-        Sign In
-      </Button>
-      <Button
-        icon=""
-        mode="outlined"
-        onPress={() => router.push("/(auth)/signup")}
-        style={styles.button}
-      >
-        Register
-      </Button>
+      <View style={styles.buttonContainer}>
+        <Button
+          icon="paw"
+          mode="contained"
+          onPress={() => router.push("/sightings/new")}
+          contentStyle={{ width: "100%" }}
+          style={styles.button}
+        >
+          Report a Pet Sighting
+        </Button>
+        <Button
+          icon="paw"
+          mode="outlined"
+          onPress={() => router.push("/sightings/")}
+          contentStyle={{ width: "100%" }}
+          style={styles.button}
+        >
+          View Lost Pet Sightings
+        </Button>
+        <Button
+          icon=""
+          mode="outlined"
+          onPress={() => router.push("/(auth)/signin")}
+          style={styles.button}
+        >
+          Sign In
+        </Button>
+        <Button
+          icon=""
+          mode="outlined"
+          onPress={() => router.push("/(auth)/signup")}
+          style={styles.button}
+        >
+          Register
+        </Button>
+      </View>
       <FAB
         icon="message-outline"
         label="Report"
@@ -58,19 +62,24 @@ export default function PublicHome() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 80,
     paddingHorizontal: 24,
     alignItems: "center",
     backgroundColor: "#fff",
+    flexDirection: "column",
   },
   logo: {
     width: "100%",
-    marginBottom: 40,
-    marginTop: 40,
     resizeMode: "contain",
   },
   button: {
     width: "100%",
     marginBottom: 16,
+  },
+  buttonContainer: {
+    width: "100%",
+  },
+  logoContainer: {
+    width: "100%",
+    alignItems: "center",
   },
 });
