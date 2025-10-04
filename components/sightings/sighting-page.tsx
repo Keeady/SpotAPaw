7d21cf1d06061fa39f14a3026f983c7909e55d25
Camaria Bevavy <cabevavy@microsoft.com>
2025-10-04 13:44:48 -0700

    Update SightingPage to improve UI and functionality for filtering sightings

--- components/sightings/sighting-page.tsx
+++ components/sightings/sighting-page.tsx
@@ -8,7 +8,7 @@ import { PetSighting } from "@/model/sighting";
 import { AuthContext } from "../Provider/auth-provider";
 import { isValidUuid } from "../util";
 
-const RADIUSKM = 10;
+const RADIUSKM = 20;
 
 export default function SightingPage({
   renderer,
@@ -17,7 +17,7 @@ export default function SightingPage({
 }) {
   const [sightings, setSightings] = useState<PetSighting[]>([]);
   const [loading, setLoading] = useState(false);
-  const [filterNearby, setFilterNearby] = useState(false);
+  const [filterNearby, setFilterNearby] = useState(true);
   const [location, setLocation] = useState<SightingLocation>();
   const { user } = useContext(AuthContext);
 
@@ -35,12 +35,33 @@ export default function SightingPage({
   }, [filterNearby, location, user]);
 
   return (
-    <View className="flex-1 p-4">
-      <Button onPress={() => setFilterNearby(!filterNearby)}>
-        {filterNearby ? "Show All Recent Sightings" : "Show Nearby Sightings"}
-      </Button>
-
-      {loading ? <ActivityIndicator size="large" /> : renderer(sightings)}
+    <View style={{ flex: 1, padding: 5 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: 10,
+        }}
+      >
+        <Button
+          mode="text"
+          onPress={() => setFilterNearby(!filterNearby)}
+          disabled={loading}
+        >
+          {filterNearby
+            ? loading
+              ? "Loading Nearby Sightings..."
+              : "Show All Recent Sightings"
+            : loading
+            ? "Loading All Recent Sightings..."
+            : "Show Nearby Sightings"}
+        </Button>
+        {loading ? <ActivityIndicator size="small" /> : ""}
+      </View>
+      <View style={{ flex: 1, backgroundColor: "white" }}>
+        {loading ? null : renderer(sightings)}
+      </View>
     </View>
   );
 }
