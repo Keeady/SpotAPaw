8eb759841ad3cba02cc3ffa1f8e6184254382fa3
Camaria Bevavy <cabevavy@microsoft.com>
2025-11-30 15:40:05 -0800

    Save sighting from chat bot

---
+++ components/sightings/sightings-crud.ts
@@ -0,0 +1,107 @@
+import { PetSighting, PetSightingFromChat } from "@/model/sighting";
+import { supabase } from "../supabase-client";
+import { PetReportData } from "./sighting-interface";
+import * as chrono from "chrono-node";
+import { isValidUuid } from "../util";
+
+type ChatBotSightingResult = {
+  error: string;
+  reportId: string;
+};
+
+export const saveChatBotSighting = async (
+  report: PetReportData,
+  uploadImage: (uri: string, callback: (uri: string) => void) => Promise<void>,
+  callback: (result: ChatBotSightingResult) => void
+) => {
+  const finalSighting = {
+    name: report.petName,
+    colors: report.color,
+    features: report.distinctiveFeatures,
+    species: report.petType,
+    breed: report.breed,
+    gender: report.gender,
+    photo: "",
+    last_seen_long: report.lastSeenLocationLng,
+    last_seen_lat: report.lastSeenLocationLat,
+    last_seen_location: report.lastSeenLocation,
+    last_seen_time: convertTime(report.lastSeenTime || ""),
+    pet_id: isValidUuid(report.petId) ? report.petId : null,
+    note: JSON.stringify({
+      notes: report.notes || "",
+      collar:
+        report.hasCollar === "yes_collar"
+          ? `Yes, ${report.collarDescription}`
+          : "No",
+      harness:
+        report.hasCollar === "yes_harness"
+          ? `Yes, ${report.collarDescription}`
+          : "No",
+      tags:
+        report.hasCollar === "yes_tags"
+          ? `Yes, ${report.collarDescription}`
+          : "No",
+      behavior: report.petBehavior,
+    }),
+    reporter_phone: report.contactPhone,
+    reporter_name: report.contactName,
+    linked_sighting_id: isValidUuid(report.linkedSightingId)
+      ? report.linkedSightingId
+      : null,
+  };
+
+  // Upload image if exists and then save sighting
+  if (report.photo) {
+    uploadImage(report.photo, (url) => {
+      finalSighting.photo = url || "";
+      saveSightingInfo(finalSighting, callback);
+    });
+  } else {
+    saveSightingInfo(finalSighting, callback);
+  }
+};
+
+async function saveSightingInfo(
+  finalSighting: any,
+  callback: (result: ChatBotSightingResult) => void
+) {
+  let errorMsg = "";
+  let reportId = "";
+
+  const { error, data } = await supabase
+    .from("sightings")
+    .insert([finalSighting])
+    .select();
+
+  if (error) {
+    console.error("Error saving sighting:", error);
+    errorMsg = "Error saving report. Please try again.";
+  }
+
+  if (data) {
+    reportId = data[0].id;
+  }
+
+  callback({
+    error: errorMsg,
+    reportId,
+  });
+}
+
+function convertTime(time: string) {
+  // Convert time to ISO 8601 format if possible
+  let lastSeenTime = "";
+  if (time) {
+    try {
+      const convertedDateTime = chrono.parseDate(time, new Date(), {
+        forwardDate: false,
+      });
+      lastSeenTime =
+        convertedDateTime?.toISOString() || new Date().toISOString();
+    } catch {
+      lastSeenTime = new Date().toISOString();
+    }
+  }
+
+  return lastSeenTime;
+}
