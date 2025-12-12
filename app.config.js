950e903075bcb5cb27ce24435fbfa5f78ce919c6
Camaria Bevavy <cabevavy@microsoft.com>
2025-12-11 20:02:06 -0800

    Fix key configs and camera permission

--- app.config.js
+++ app.config.js
@@ -2,6 +2,16 @@ import "dotenv/config";
 export default ({ config }) => {
   return {
     ...config,
+    ...{
+      android: {
+        ...(config.android || {}),
+        config: {
+          googleMaps: {
+            apiKey: process.env.EXPO_GOOGLE_MAP_API_KEY,
+          },
+        },
+      },
+    },
     extra: {
       eas: {
         projectId: "f8aec9d1-5a47-4313-90ce-b3793194513d",
@@ -9,7 +19,7 @@ export default ({ config }) => {
       EXPO_GEN_AI_KEY: process.env.EXPO_GEN_AI_KEY,
       EXPO_PUBLIC_SUPABASE_URL: process.env.EXPO_PUBLIC_SUPABASE_URL,
       EXPO_PUBLIC_SUPABASE_ANON_KEY: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
-      EXPO_GOOGLE_MAP_API_KEY: process.env.EXPO_GOOGLE_MAP_API_KEY
+      EXPO_GOOGLE_MAP_API_KEY: process.env.EXPO_GOOGLE_MAP_API_KEY,
     },
   };
 };
