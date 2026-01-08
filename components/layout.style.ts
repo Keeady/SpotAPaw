cfbe83e1114a05c66ffdfde232a1512e538f5536
Camaria Bevavy <cabevavy@microsoft.com>
2026-01-07 22:26:19 -0800

    remove left margin

--- components/layout.style.ts
+++ components/layout.style.ts
@@ -3,9 +3,10 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   logo: {
     width: 100,
-    marginLeft: 24,
+    marginLeft: 0,
     resizeMode: "contain",
     height: 50,
+    backgroundColor: "red"
   },
   button: {
     marginRight: 12,
