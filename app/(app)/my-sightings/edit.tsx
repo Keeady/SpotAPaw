9e8c7b5826d0c0b49d131f12e2b7ca7cd76b5e25
Camaria Bevavy <cabevavy@microsoft.com>
2026-01-07 22:06:27 -0800

    fix editing

--- app/(app)/my-sightings/edit.tsx
+++ app/(app)/my-sightings/edit.tsx
@@ -15,7 +15,6 @@ export default function EditPetSighting() {
     sightingId: string;
     petId: string;
   }>();
-  const [pet, setPet] = useState<Pet | undefined>();
   const { user } = useContext(AuthContext);
   const [profileInfo, setProfileInfo] = useState<Pet>();
   const [sightingInfo, setSightingInfo] = useState<PetSighting>();
@@ -34,8 +33,7 @@ export default function EditPetSighting() {
       .select("*")
       .eq("id", petId)
       .single()
-      .then(({ data, error }) => {
-        setPet(data);
+      .then(({ data }) => {
         setProfileInfo(data);
       });
   }, [petId]);
@@ -50,7 +48,7 @@ export default function EditPetSighting() {
       .select("*")
       .eq("id", sightingId)
       .single()
-      .then(({ data, error }) => {
+      .then(({ data }) => {
         setSighting(data);
         setSightingInfo(data);
       });
@@ -119,7 +117,7 @@ export default function EditPetSighting() {
     saveSightingPhoto,
     setProfileInfo,
     setSightingInfo,
-    pet,
+    profileInfo,
     sighting,
     true
   );
