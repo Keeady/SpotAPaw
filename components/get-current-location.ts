3da6a2ec922eec378d0ebb2db99b37b695e2a911
Camaria Bevavy <bcamaria@gmail.com>
2026-01-24 01:02:12 -0800

    Fix location permission not getting applied  (#41)
    * fix location permission not getting applied
    
    * Update wording
    
    * fix
    
    * fix wording
    
    ---------
    
    Co-authored-by: Camaria Bevavy <cabevavy@microsoft.com>

--- components/get-current-location.ts
+++ components/get-current-location.ts
@@ -27,7 +27,7 @@ export async function getCurrentLocationV4() {
     log(`getCurrentLocationV4: ${e}`);
     Alert.alert(
       "Location Permission Required",
-      "Please enable location or type the address instead.",
+      "Please grant Location permission in your device settings.",
       [
         { text: "Cancel", style: "cancel" },
         { text: "Open Settings", onPress: () => Linking.openSettings() },
