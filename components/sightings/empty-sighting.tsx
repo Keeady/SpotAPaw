326795415fdc90677889399130810ee40f753a61
Camaria Bevavy <bcamaria@gmail.com>
2026-01-24 21:25:50 -0800

    show map instead if location is not granted (#42)
    Co-authored-by: Camaria Bevavy <cabevavy@microsoft.com>

--- components/sightings/empty-sighting.tsx
+++ components/sightings/empty-sighting.tsx
@@ -1,118 +1,98 @@
 import React from "react";
-import { Linking, View } from "react-native";
-import { Button, Icon, Text } from "react-native-paper";
+import { StyleSheet, View } from "react-native";
+import { Icon, Text } from "react-native-paper";
 import { ShowHappyDogAnimation } from "@/components/animate";
+import DropPinOnMap from "../map-util";
+import { SightingLocation } from "../get-current-location";
 
 type EmptySightingProps = {
   error: string;
-  enableFromSettings: boolean;
-  reloadPage: () => void;
   hasLocation: boolean;
+  onLocationSelected: (location?: SightingLocation) => void;
 };
 
 export const EmptySighting = ({
   error,
-  enableFromSettings,
-  reloadPage,
   hasLocation,
+  onLocationSelected,
 }: EmptySightingProps) => (
-  <View
-    style={{
-      alignContent: "center",
-      alignItems: "center",
-      flexDirection: "column",
-      gap: 20,
-      flex: 1,
-      paddingTop: 100,
-    }}
-  >
-    <View>
-      <ShowHappyDogAnimation />
-    </View>
-
-    {error ? (
-      <View
-        style={{
-          flexDirection: "row",
-          alignItems: "center",
-          flexWrap: "wrap",
-          flex: 1,
-          paddingHorizontal: 8,
-          paddingVertical: 4,
-          gap: 8,
-        }}
-      >
-        <Icon source="alert-circle-outline" size={32} color="red" />
-        <Text
-          variant="bodyLarge"
-          style={{
-            flex: 1,
-            color: "red",
-            flexWrap: "wrap",
-          }}
-        >
-          {error}
-        </Text>
-      </View>
-    ) : (
-      <View
-        style={{
-          flexDirection: "row",
-          alignItems: "center",
-          flexWrap: "wrap",
-          flex: 1,
-          paddingHorizontal: 8,
-          paddingVertical: 4,
-          gap: 8,
-        }}
-      >
-        <Icon source="paw-outline" size={32} color="green" />
-        <Text
-          variant="bodyLarge"
-          style={{
-            flex: 1,
-            flexWrap: "wrap",
-            color: "green",
-          }}
-        >
-          No pet sightings to display in your area
-        </Text>
+  <View style={styles.container}>
+    {!hasLocation && (
+      <View style={styles.mapContainer}>
+        <View style={styles.iconWithText}>
+          <Icon source="alert-circle-outline" size={32} color="blue" />
+          <Text variant="labelLarge" style={styles.infoText}>
+            Tap on the map to select your location. Results will be shown near
+            this location.
+          </Text>
+        </View>
+        <DropPinOnMap handleActionButton={onLocationSelected} />
       </View>
     )}
 
-    {enableFromSettings && !hasLocation && (
-      <Button
-        mode="contained"
-        onPress={() => {
-          Linking.openSettings();
-        }}
-        compact={true}
-        style={{ paddingHorizontal: 10, marginTop: 10 }}
-      >
-        Enable Location Access
-      </Button>
-    )}
-    {enableFromSettings && (
-      <Button
-        mode="outlined"
-        onPress={() => {
-          reloadPage();
-        }}
-        compact={true}
-        style={{ paddingHorizontal: 10, marginTop: 10 }}
-      >
-        Reload
-      </Button>
-    )}
-
-    {enableFromSettings && (
-      <Text
-        variant="labelMedium"
-        style={{ paddingHorizontal: 10, marginTop: 10 }}
-      >
-        Tap "Enable Location Access" button to open Settings, grant Location
-        permission, then tap "Reload" button to apply changes.
-      </Text>
+    {hasLocation && (
+      <View style={styles.mapContainer}>
+        <ShowHappyDogAnimation />
+        {error ? (
+          <View style={styles.iconWithText}>
+            <Icon source="alert-circle-outline" size={32} color="red" />
+            <Text variant="bodyLarge" style={styles.errorText}>
+              {error}
+            </Text>
+          </View>
+        ) : (
+          <View style={styles.iconWithText}>
+            <Icon source="paw-outline" size={32} color="green" />
+            <Text variant="bodyLarge" style={styles.successText}>
+              No pet sightings to display in your area
+            </Text>
+          </View>
+        )}
+      </View>
     )}
   </View>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    alignContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    gap: 20,
+    flex: 1,
+    paddingTop: 100,
+  },
+  mapContainer: {
+    flexDirection: "column",
+    alignItems: "center",
+    flexWrap: "wrap",
+    flex: 1,
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    gap: 8,
+  },
+  iconWithText: {
+    flexDirection: "row",
+    alignItems: "center",
+    flexWrap: "wrap",
+    flex: 1,
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    gap: 8,
+  },
+  successText: {
+    flex: 1,
+    flexWrap: "wrap",
+    color: "green",
+  },
+  errorText: {
+    flex: 1,
+    color: "red",
+    flexWrap: "wrap",
+  },
+  infoText: {
+    flex: 1,
+    color: "blue",
+    flexWrap: "wrap",
+  },
+});
